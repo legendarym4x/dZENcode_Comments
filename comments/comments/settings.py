7686f2b68abbd6ddf72b091dae0769fe94f4f313
legendarym4x <maxwel842@gmail.com>
2024-09-18 20:18:31 +0300

    prepare_to_deploy_heroku_1

--- comments/comments/settings.py
+++ comments/comments/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = "comments.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default='DATABASE_URL')
 }
 
 
